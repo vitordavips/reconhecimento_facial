3dbc81bd04ffc8ad4103c426a87265bc11e8a63e
vitordavips <vitordavidps@gmail.com>
2025-11-30 01:42:53 -0400

    criando conexão com a webcam e criando o reconhecimento facial

--- src/main.py
+++ src/main.py
@@ -7,7 +7,22 @@ solucao_reconhcimento_rosto = mp.solutions.face_detection
 reconhecimento_rostos = solucao_reconhcimento_rosto.FaceDetection()
 desenho = mp.solutions.drawing_utills
 
+while True:
+    #Ler as informações da webcam
+    verificador, frame = webcam.read()
+    if not verificador:
+        break
 
+    #reconhecer os rostos que tem ali dentro
+    lista_rostos = reconhecimento_rostos.process(frame)
+
+    if lista_rostos.detections:
+        for rosto in lista_rostos.detections:
+            #desenhar os rostos na imagem
+            desenho.draw_detection(frame, rosto)
+
+        #quando apertar ESC, para o loop
+webcam.release()
 
 
 
